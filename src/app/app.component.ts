8762d0a6706d5c16c0d1c1df621475d61284e46d
RevanthRavi25 <revanthravi5@gmail.com>
2024-03-15 15:49:11 +0530

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
-
+import { NgModule } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
+  stud_Id: any="";
+  stud_name: any="";
+  stud_rolnum: any="";
+  stud_birtday: any="";
   student:any;
     constructor(private dataService: AppService) {}
   
     ngOnInit(): void {
+    this.readdata()
+    }
+    readdata(){
       this.dataService.fetchData().subscribe(data => { 
-        this.student=data
-        console.log(data); // Process your data here
-      });
+        //  add this.student=data
+            this.student=data
+            console.log(data); // Process your data here
+          });
+    };
+    onSubmit() {
+      this.dataService.addData({   
+        StudentId:this.stud_Id,
+        Name:this.stud_name,
+        Roll:this.stud_rolnum,
+        Birthday:this.stud_birtday,
+      }).subscribe(
+        response => {
+          console.log('Data added successfully:', response);
+          this.readdata()
+          // Handle success - maybe show a success message or reset the form
+        },
+        error => {
+          console.error('Error adding data:', error);
+          this.readdata()
+          // Handle error - maybe show an error message
+        }
+      );
     }
   }
   
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -12,4 +12,8 @@ export class AppService {
   fetchData(): Observable<any> {
     return this.http.get<any>('http://172.16.4.88:3000/api/findall');
   }
+  addData(data: any): Observable<any> {
+    console.log(data);
+    return this.http.post<any>(`http://172.16.4.88:3000/api/save`, data);
+  }
 }
